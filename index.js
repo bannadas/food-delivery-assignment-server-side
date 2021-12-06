c33cf0d86a7202451516b20171b31237ac237246
bannadas <bannadaso39@gmail.com>
2021-12-07 00:21:23 +0600

    edit removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-// something
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2rtmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
