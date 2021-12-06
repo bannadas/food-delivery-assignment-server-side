1e49b047234d6bb67227eb6f91aef1a0460e7c70
bannadas <bannadaso39@gmail.com>
2021-12-07 00:20:45 +0600

    edit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
-
+// something
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2rtmd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -58,6 +58,16 @@ async function run() {
             const cursor = orderCollection.find({});
             const allOrders = await cursor.toArray();
             res.send(allOrders);
+        })
+
+        // get my orders api
+        app.get('/allorders/:userEmail', async (req,res)=>{
+            const email = req.params.userEmail;
+            const query = {userEmail:email}
+            const cursor = orderCollection.find(query)
+            const result = await cursor.toArray();
+            res.json(result);
+            console.log(result);
         })
     }
 
